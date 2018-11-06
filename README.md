f3f6a3f226a36318a9d107d83c552889b8793ffd
garricknorthover <garricknorthover@gmail.com>
2018-11-07 09:27:50 +1300

    added axios defaults and updated readme

--- README.md
+++ README.md
@@ -32,4 +32,26 @@ axios
   .catch(error => console.log(error))
 ```
 
+And as I just learned 5 minutes ago, you can configure you requests with defaults.
+```javascript
+const axios = require('axios')
+
+require('dotenv').config()
+
+axios.defaults.baseURL = 'https://www.eventbriteapi.com/v3/'
+axios.defaults.params = { token: process.env.MYTOKEN }
+
+axios
+  .get('users/me')
+  .then(res => console.log(res.data))
+  .catch(error => console.log(error))
+  
+axios
+  .get('users/me')
+  .then(res => console.log(res.data.name))
+  .catch(error => console.log(error))
+```
+
+
 @babel/register package is installed so you can use Quokka
+I like Quokka  https://quokkajs.com/
--- src/api.js
+++ src/api.js
@@ -1,23 +1,16 @@
 const axios = require('axios')
 
 require('dotenv').config()
-const MYTOKEN = process.env.MYTOKEN
 
-// axios
-//   .get('https://www.eventbriteapi.com/v3/users/me/', {
-//     params: {
-//       token: MYTOKEN
-//     }
-//   })
-//   .then(res => console.log(res.data))
-//   .catch(error => console.log(error))
+axios.defaults.baseURL = 'https://www.eventbriteapi.com/v3/'
+axios.defaults.params = { token: process.env.MYTOKEN }
 
-
-  axios
-  .get('https://www.eventbriteapi.com/v3/users/me/owned_events', {
-    params: {
-      token: MYTOKEN
-    }
-  })
+axios
+  .get('users/me')
   .then(res => console.log(res.data))
+  .catch(error => console.log(error))
+  
+axios
+  .get('users/me')
+  .then(res => console.log(res.data.name))
   .catch(error => console.log(error))
